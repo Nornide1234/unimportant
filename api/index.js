fe9c7759f75409ea4f4505a259c064edb789bf5a
Nornide1234 <joe987110@gmail.com>
2026-07-19 01:05:06 -0500

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const { app, ready } = require('../app');
-
-// Vercel calls this per-request instead of running app.listen() - an Express
-// app is itself a valid (req, res) handler, so awaiting the DB-init promise
-// first and then handing off to it is all that's needed.
-module.exports = async (req, res) => {
-    await ready;
-    app(req, res);
-};
